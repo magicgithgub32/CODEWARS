2c2843d8d3d174b0c705118011b3961da07ba44e
Rubcs <116936603+magicgithgub32@users.noreply.github.com>
2025-05-05 15:37:06 +0200

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-create-the-rooms/grasshopper-create-the-rooms.js
@@ -0,0 +1,18 @@
+// Add rooms here
+var rooms = {
+ 'room1':{
+  name: 'room1',
+  descr: 'desc1',
+  completed: true
+} , 
+    'room2':{
+      name: 'room2',
+  descr: 'desc2',
+  completed: true
+    },
+    'room3': {
+    name: 'room3',
+  descr: 'desc3',
+  completed: true
+      }
+}
