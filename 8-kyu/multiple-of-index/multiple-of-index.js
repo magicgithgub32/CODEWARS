610d5712a50a0de35959933e43f44c9a0c044cf9
Rubcs <116936603+magicgithgub32@users.noreply.github.com>
2025-05-05 16:28:07 +0200

    Add solution - CodeHub

---
+++ 8-kyu/multiple-of-index/multiple-of-index.js
@@ -0,0 +1,14 @@
+function multipleOfIndex(array) {
+  let newArr = []
+  
+  
+  
+  for(let i = 0; i< array.length; i++) {
+    if(array[i] % i == 0) {
+      newArr.push(array[i])
+    } else if (array[i] === 0) {
+      newArr.push(array[i])
+    }
+  }
+  return newArr
+}
