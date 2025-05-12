9799329d0dbf5b5f4369e2f2b582b003638b7b7c
Rubcs <116936603+magicgithgub32@users.noreply.github.com>
2025-05-12 10:10:35 +0200

    Add solution - CodeHub

---
+++ 7-kyu/find-the-capitals-1/find-the-capitals-1.js
@@ -0,0 +1,18 @@
+ 
+var capitals = function (word) {
+  
+  let arr = word.split("")
+  
+  let arrSol = []
+  
+  let capWord = word.toUpperCase()
+  
+  let arrCap = capWord.split("")
+  
+  for(letter of arr) {
+    if (letter === )
+  }
+  
+  
+  
+};
