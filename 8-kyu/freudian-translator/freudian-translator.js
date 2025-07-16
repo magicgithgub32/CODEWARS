aa96ad61c6b6b772492f5a7378f73a9b748ce74b
Rubcs <116936603+magicgithgub32@users.noreply.github.com>
2025-07-16 12:45:06 +0200

    Add solution - CodeHub

---
+++ 8-kyu/freudian-translator/freudian-translator.js
@@ -0,0 +1,15 @@
+function toFreud(string) {
+let result = []
+​
+let arr = string.split(" ")
+​
+if (string === "") {
+  return string
+}
+​
+for (let i = 0; i < arr.length; i++) {
+  result.push('sex')
+}
+  return result.join(" ")
+  
+}
